7e62178cb2fcefabfcd6b58143ed9e5923df6c20
Алексей <rebuilding.c@gmail.com>
2021-12-12 21:02:35 +0300

    Delete TestSQL.sql

--- TestSQL.sql
+++
@@ -1,8 +0,0 @@
-SELECT goods.name, prices.value FROM goods
-JOIN Prices ON Prices.goods_id=Goods.id
-WHERE prices.value = (SELECT MAX(prices.value) FROM Prices)
---изменяем порядок сортировки ASC, DESC
-SELECT name,supplier_id FROM Goods ORDER BY supplier_id ASC
-SELECT name,supplier_id FROM Goods ORDER BY supplier_id DESC
-
-
